df3c61408b2f84754e8c98aabeca8b7c41784f10
Liam St-Onge-Amoretti <liamstonge123@hotmail.ca>
2024-12-31 08:47:34 -0500

    setup website awards showcase with deno, vite & tailwind

---
+++ Awards/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main><h1 className='text-5xl text-orange-500 font-bold'>welcome to awards</h1></main>
+  )
+}
+
+export default App
