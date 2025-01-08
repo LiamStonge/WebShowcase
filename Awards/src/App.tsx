b4cb2e891edd33dd07380f04b64e35bfd55db479
Liam St-Onge-Amoretti <liamstonge123@hotmail.ca>
2025-01-08 15:19:59 -0500

    added features

--- Awards/src/App.tsx
+++ Awards/src/App.tsx
@@ -1,8 +1,56 @@
 import About from './components/About.tsx';
+import { FeatureData, Features } from './components/Features.tsx';
 import Hero from './components/Hero.tsx';
 import Navbar from './components/Navbar.tsx';
 
 const navItems = ['Nexus', 'Vault', 'Prologue', 'about', 'contact'];
+
+const MyFeatureData: FeatureData[] = [
+    {
+        src: 'videos/feature-1.mp4',
+        title: (
+            <>
+                radi<b>n</b>t
+            </>
+        ),
+        description:
+            'A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure.',
+        isComingSoon: true,
+    },
+    {
+        src: 'videos/feature-2.mp4',
+        title: (
+            <>
+                zig<b>m</b>a
+            </>
+        ),
+        description:
+            'An anime and gaming-inspired NFT collection - the IP primed for expansion.',
+        isComingSoon: false,
+    },
+    {
+        src: 'videos/feature-3.mp4',
+        title: (
+            <>
+                n<b>e</b>xus
+            </>
+        ),
+        description:
+            'A gamified social hub, adding a new dimension of play to social interaction for Web3 communities.',
+        isComingSoon: false,
+    },
+    {
+        src: 'videos/feature-4.mp4',
+        title: (
+            <>
+                az<b>u</b>l
+            </>
+        ),
+        description:
+            'A cross-world AI Agent - elevating your gameplay to be more fun and productive.',
+        isComingSoon: false,
+    },
+];
 
 const App = () => {
     return (
@@ -10,6 +58,7 @@ const App = () => {
             <Navbar navItems={navItems} />
             <Hero />
             <About />
+            <Features {...MyFeatureData} />
         </main>
     );
 };
--- Awards/src/components/About.tsx
+++ Awards/src/components/About.tsx
@@ -20,7 +20,7 @@ const About = () => {
 
         clipAnimation.to('.mask-clip-path', {
             width: '100vw',
-            height: '100vw',
+            height: '100vh',
             borderRadius: 0,
         });
     });
