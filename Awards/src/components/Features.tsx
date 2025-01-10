4ba8fde680335748303d3efba00579d125dc818a
Liam St-Onge-Amoretti <liamstonge123@hotmail.ca>
2025-01-10 08:08:21 -0500

    finished features component

--- Awards/src/components/Features.tsx
+++ Awards/src/components/Features.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, useRef, useState } from 'react';
 import { TiLocationArrow } from 'react-icons/ti';
 
 export type FeatureData = {
@@ -33,6 +34,47 @@ const BentoCard = (
     );
 };
 
+const BentoTilt = (props: { children: React.ReactNode; className: string }) => {
+    const [transformStyle, setTransformStyle] = useState('');
+    const itemRef: React.RefObject<HTMLDivElement | null> = useRef(null);
+
+    const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+        if (!itemRef.current) {
+            return;
+        }
+
+        const { left, top, width, height } = itemRef.current
+            .getBoundingClientRect();
+
+        const relativeX = (e.clientX - left) / width;
+        const relativeY = (e.clientY - top) / height;
+
+        const tiltX = (relativeY - 0.5) * 5;
+        const tiltY = (relativeX - 0.5) * -5;
+
+        const newTransform =
+            `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+
+        setTransformStyle(newTransform);
+    };
+
+    const handleMouseLeave = () => {
+        setTransformStyle('');
+    };
+
+    return (
+        <div
+            className={props.className}
+            ref={itemRef}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{ transform: transformStyle }}
+        >
+            {props.children}
+        </div>
+    );
+};
+
 export const Features = (props: FeatureData[]) => {
     return (
         <section className='bg-black pb-52'>
@@ -48,24 +90,24 @@ export const Features = (props: FeatureData[]) => {
                     </p>
                 </div>
 
-                <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[64vh]'>
+                <BentoTilt className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[64vh]'>
                     <BentoCard
                         {...props[0]}
                     />
-                </div>
+                </BentoTilt>
 
                 <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7'>
-                    <div className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
+                    <BentoTilt className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
                         <BentoCard {...props[1]} />
-                    </div>
-                    <div className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0'>
+                    </BentoTilt>
+                    <BentoTilt className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0'>
                         <BentoCard {...props[2]} />
-                    </div>
-                    <div className='bento-tilt_1 me-14 md:col-span-1 md:me-0'>
+                    </BentoTilt>
+                    <BentoTilt className='bento-tilt_1 me-14 md:col-span-1 md:me-0'>
                         <BentoCard {...props[3]} />
-                    </div>
+                    </BentoTilt>
 
-                    <div className='bento-tilt_2'>
+                    <BentoTilt className='bento-tilt_2'>
                         <div className='flex size-full flex-col justify-between bg-violet-300 p-5'>
                             <h1 className='bento-title special-font max-w-64 text-black'>
                                 M<b>o</b>re co<b>m</b>ing s<b>o</b>on!
@@ -73,8 +115,8 @@ export const Features = (props: FeatureData[]) => {
 
                             <TiLocationArrow className='m-5 scale-[5] self-end' />
                         </div>
-                    </div>
-                    <div className='bento-tilt_2'>
+                    </BentoTilt>
+                    <BentoTilt className='bento-tilt_2'>
                         <video
                             src='videos/feature-5.mp4'
                             loop
@@ -82,7 +124,7 @@ export const Features = (props: FeatureData[]) => {
                             autoPlay
                             className='size-full object-cover object-center'
                         />
-                    </div>
+                    </BentoTilt>
                 </div>
             </div>
         </section>
